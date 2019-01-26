9fc99220f579e66765aa463737e6042856b312ac
HellForGate <995171801@qq.com>
2019-01-26 11:46:13 +0800

    修改 '合并两个有序链表'

--- app/src/main/java/com/sflin/algorithmic/linkedList/EasyLinkedListUtils.java
+++ app/src/main/java/com/sflin/algorithmic/linkedList/EasyLinkedListUtils.java
@@ -118,31 +118,30 @@ public class EasyLinkedListUtils {
      * 示例
      * 输入：1->2->4, 1->3->4
      * 输出：1->1->2->3->4->4
+     *
      */
     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l2 == null) return l1;
-        if (l1 == null) return l2;
-        ListNode p1 = l1;
-        ListNode p2 = l2;
-        ListNode start = l1.val < l2.val ? l1 : l2;
-        do {
-            if (p2.val <= p1.val) {
-                while (p2.next != null && p2.next.val <= p1.val) {
-                    p2 = p2.next;
-                }
-                l2 = p2.next;
-                p2.next = p1;
+        // 类似归并排序中的合并过程
+        ListNode dummyHead = new ListNode(0);
+        ListNode cur = dummyHead;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                cur = cur.next;
+                l1 = l1.next;
             } else {
-                while (p1.next != null && p1.next.val < p2.val) {
-                    p1 = p1.next;
-                }
-                l2 = p2.next;
-                p2.next = p1.next;
-                p1.next = p2;
+                cur.next = l2;
+                cur = cur.next;
+                l2 = l2.next;
             }
-            p2 = l2;
-        } while (p2 != null);
-        return start;
+        }
+        // 任一为空，直接连接另一条链表
+        if (l1 == null) {
+            cur.next = l2;
+        } else {
+            cur.next = l1;
+        }
+        return dummyHead.next;
     }
 //    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 //        if (l1 == null) return l2;

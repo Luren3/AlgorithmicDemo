4ab0307c46380f89b2beb865377faec08f936124
HellForGate <995171801@qq.com>
2019-02-18 16:45:18 +0800

    增加 算法'第一个错误的版本'

--- app/src/main/java/com/sflin/algorithmic/sort_and_serarch/EasySortAndSearchUtils.java
+++ app/src/main/java/com/sflin/algorithmic/sort_and_serarch/EasySortAndSearchUtils.java
@@ -24,7 +24,7 @@ public class EasySortAndSearchUtils {
      *
      * 输出: [1,2,2,3,5,6]
      */
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
         int[] newArr = new int[m+n];
         for (int i = 0; i < m;i++){
             newArr[i] = nums1[i];
@@ -36,5 +36,38 @@ public class EasySortAndSearchUtils {
         for (int i = 0; i < newArr.length;i++){
             nums1[i] = newArr[i];
         }
+    }
+
+    /**
+     * 第一个错误的版本
+     *
+     * @param n
+     * @return
+     *
+     * 示例
+     * 给定 n = 5，并且 version = 4 是第一个错误的版本。
+     *
+     * 调用 isBadVersion(3) -> false
+     * 调用 isBadVersion(5) -> true
+     * 调用 isBadVersion(4) -> true
+     *
+     * 所以，4 是第一个错误的版本。
+     */
+    /* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+    public static int firstBadVersion(int n) {
+        int low = 1, high = n;
+        while(low < high){
+            int mid = low + (high - low)/2;
+            if(isBadVersion(mid)) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return high;
+    }
+    private static boolean isBadVersion(int version){
+        return false;
     }
 }

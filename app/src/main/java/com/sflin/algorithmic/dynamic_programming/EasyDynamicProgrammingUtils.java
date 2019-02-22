583712702403ea983a25c7ef961ff69a66e20e8c
HellForGate <995171801@qq.com>
2019-02-22 16:08:54 +0800

    增加 算法'打家劫舍'

--- app/src/main/java/com/sflin/algorithmic/dynamic_programming/EasyDynamicProgrammingUtils.java
+++ app/src/main/java/com/sflin/algorithmic/dynamic_programming/EasyDynamicProgrammingUtils.java
@@ -119,4 +119,42 @@ public class EasyDynamicProgrammingUtils {
 //        }
 //        return max;
 //    }
+
+    /**
+     * 打家劫舍
+     *
+     * @param nums
+     * @return
+     *
+     * 示例
+     * 输入: [1,2,3,1]
+     * 输出: 4
+     * 解释: 偷窃 1 号房屋 (金额 = 1) ，然后偷窃 3 号房屋 (金额 = 3)。
+     *      偷窃到的最高金额 = 1 + 3 = 4 。
+     */
+    public static int rob(int[] nums) {
+        int length = nums.length;
+        if (length <= 1){
+            return length == 0 ? 0 : nums[0];
+        }
+        int[] dp = new int[length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0],nums[1]);
+        for (int i=2;i<length;i++){
+            dp[i] = Math.max(nums[i]+dp[i-2],dp[i-1]);
+        }
+        return dp[length-1];
+    }
+//    public static int rob(int[] nums) {
+//        int robEven = 0,robOdd = 0;
+//        for (int i=0;i<nums.length;i++){
+//            if (i % 2 == 0){
+//                robEven = Math.max(robEven+nums[i],robOdd);
+//            }else {
+//                robOdd = Math.max(robOdd+nums[i],robEven);
+//            }
+//        }
+//        return Math.max(robEven,robOdd);
+//    }
+
 }

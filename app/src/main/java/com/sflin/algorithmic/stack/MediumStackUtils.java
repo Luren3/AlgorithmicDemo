c9314240fe8e67a7eb9da5d6feb6a2c2b1567203
HellForGate <995171801@qq.com>
2019-04-01 18:41:43 +0800

    增加 算法'前K个高频元素'

--- app/src/main/java/com/sflin/algorithmic/stack/MediumStackUtils.java
+++ app/src/main/java/com/sflin/algorithmic/stack/MediumStackUtils.java
@@ -1,6 +1,9 @@
 package com.sflin.algorithmic.stack;
 
-import java.util.PriorityQueue;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+import java.util.*;
 
 /**
  * Created by MagicFrost
@@ -35,4 +38,44 @@ public class MediumStackUtils {
 //        return nums[nums.length - k];
 //    }
 
+    /**
+     * 前K个高频元素
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public static List<Integer> topKFrequent(int[] nums, int k) {
+        List<Integer> list = new ArrayList<>();
+
+        PriorityQueue<Map.Entry<Integer,Integer>> pq = new PriorityQueue<>(new Comparator<Map.Entry<Integer, Integer>>() {
+            @Override
+            public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
+                return o2.getValue()-o1.getValue();
+            }
+        });
+
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i]))
+                map.put(nums[i], map.get(nums[i]) + 1);
+            else
+                map.put(nums[i], 1);
+        }
+
+        Set<Map.Entry<Integer,Integer>> set = map.entrySet();
+
+        for (Map.Entry<Integer,Integer> entry : set) {
+            pq.add(entry);
+        }
+
+
+        for (int i = 0; i < k; i++) {
+            list.add(pq.poll().getKey());
+        }
+
+        return list;
+    }
 }

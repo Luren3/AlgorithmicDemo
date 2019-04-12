bf425d88a4dd7d513eeaef4c5b3f49ec77d62c51
HellForGate <995171801@qq.com>
2019-04-12 10:46:44 +0800

    增加 算法'相交链表'

--- app/src/main/java/com/sflin/algorithmic/linkedList/EasyLinkedListUtils.java
+++ app/src/main/java/com/sflin/algorithmic/linkedList/EasyLinkedListUtils.java
@@ -214,4 +214,46 @@ public class EasyLinkedListUtils {
         }
         return true;
     }
+
+    /**
+     * 相交链表
+     *
+     * @param headA
+     * @param headB
+     * @return
+     * 示例
+     *
+     * 输入：intersectVal = 8, listA = [4,1,8,4,5], listB = [5,0,1,8,4,5], skipA = 2, skipB = 3
+     * 输出：Reference of the node with value = 8
+     * 输入解释：相交节点的值为 8 （注意，如果两个列表相交则不能为 0）。从各自的表头开始算起，链表 A 为 [4,1,8,4,5]，链表 B 为 [5,0,1,8,4,5]。在 A 中，相交节点前有 2 个节点；在 B 中，相交节点前有 3 个节点。
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int lenA = 0,lenB = 0;
+        ListNode longNode = headA,shortNode = headB;
+        while (longNode != null){
+            lenA++;
+            longNode = longNode.next;
+        }
+        while (shortNode != null){
+            lenB++;
+            shortNode = shortNode.next;
+        }
+        if (lenA < lenB){
+            longNode = headB;
+            shortNode = headA;
+        }else {
+            longNode = headA;
+            shortNode = headB;
+        }
+        int len = Math.abs(lenA - lenB);
+        for (int i = 0;i < len;i++){
+            longNode = longNode.next;
+        }
+
+        while (longNode != shortNode){
+            longNode = longNode.next;
+            shortNode = shortNode.next;
+        }
+        return longNode;
+    }
 }

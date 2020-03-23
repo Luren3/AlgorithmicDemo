22f73313f6f24aad3f07c4d1d6987aaf9e700eb4
Luren3 <995171801@qq.com>
2020-03-23 22:30:34 +0800

    fix:每日一题 算法'链表的中间结点'

--- app/src/main/java/com/sflin/algorithmic/linkedList/EasyLinkedListUtils.java
+++ app/src/main/java/com/sflin/algorithmic/linkedList/EasyLinkedListUtils.java
@@ -160,19 +160,6 @@ public class EasyLinkedListUtils {
         }
         return true;
     }
-    // 找到链表的中间节点
-    private static ListNode findMid(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
-        }
-        ListNode slow = head;
-        ListNode fast = head;
-        while(fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        return slow;
-    }
 //    public static boolean isPalindrome(ListNode head) {
 //        List<Integer> list = new ArrayList<>();
 //        while (head != null){
@@ -255,4 +242,32 @@ public class EasyLinkedListUtils {
         }
         return longNode;
     }
+
+    /**
+     * 876. 链表的中间结点
+     *
+     * 给定一个带有头结点 head 的非空单链表，返回链表的中间结点。
+     * 如果有两个中间结点，则返回第二个中间结点。
+     *
+     * @param head
+     * @return
+     *
+     * 示例
+     * 输入：[1,2,3,4,5,6]
+     * 输出：此列表中的结点 4 (序列化形式：[4,5,6])
+     * 由于该列表有两个中间结点，值分别为 3 和 4，我们返回第二个结点。
+     */
+    public ListNode middleNode(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
 }
